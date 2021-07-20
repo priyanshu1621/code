a95882a04b593d70fe38a01a305f50e83c32da96
GABBAR-16 <82864666+GABBAR-16@users.noreply.github.com>
2021-07-21 01:00:46 +0530

    Create calculatorbyfunction.c

---
+++ calculatorbyfunction.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+ void bal(int balance)
+ {
+     printf("\nbalance: %d\n\n",balance);
+ }
+int withdraw(int balance)
+ {
+        int amt;
+         printf("\n enter the withdral amount:  ");
+         scanf("%d",&amt);
+         balance=balance-amt;
+        return balance;
+ }
+int amountdeposit(int balance)
+ {       int dep;
+         printf("\nenter the deposit amount:  ");
+         scanf("%d",&dep);
+         balance=balance+dep;
+         return balance;
+ }
+int main()
+  { 
+      int balance=10000,ch,k;
+      char p;
+ do{
+     printf("***************\n 1. Balance \n 2. Withdraw\n 3. Deposit\n 4. exit\n********** \n Enter your choise:");
+     scanf("%d",&ch);
+     
+    switch(ch)
+    {
+        case 1: bal(balance);
+                break;
+        
+        case 2: balance=withdraw(balance);
+                break;
+        
+       case 3: balance=amountdeposit(balance);
+               break;
+       
+        case 4: printf("\n thanks for using our service"); 
+                break;
+                
+       //default : printf("wrong input"); break;      
+    }
+    
+    
+   printf("u want to continue (y/n) ??");
+     scanf("%c",&p);
+    if(p=='n' || p=='N'){k=1;}
+    
+    }while(k!=1);
+ return 0;
+} 
