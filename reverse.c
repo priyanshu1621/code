e9806873a9d45a4bfee4b6b5b6cfe14b6769290c
GABBAR-16 <82864666+GABBAR-16@users.noreply.github.com>
2021-07-27 18:27:18 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,39 @@
+//reverse
+
+#include<stdio.h>
+int main()
+{
+ int n,m;
+
+printf("enter num: ");
+scanf("%d",&n);
+
+int sum=0;
+while(n>0)
+{
+m=n%10;
+sum=sum*10+m;
+n=n/10;
+}
+printf("%d",sum);
+
+
+return 0;
+}
+
+OR
+#include<stdio.h>
+int main()
+{
+ int n,m;
+
+printf("enter num: ");
+scanf("%d",&n);
+
+
+while(n>0)
+{
+m=n%10;
+printf("%d",m);
+n=n/10;
+}
